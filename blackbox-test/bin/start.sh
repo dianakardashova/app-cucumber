dfadc03458519c7b95c9d25ad91a2a8806fe11bf
Diana Kardashova <kardashova.1999@gmail.com>
2020-10-29 13:15:34 +0200

    Change architecture for the whole application: add only the changed architecture for app.

--- blackbox-test/bin/start.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/bash
-set -e
-echo "$(pwd) /usr/local/bundle/bin/cucumber ./features/ "$@""
-mkdir -p ../test-reports
-/usr/local/bundle/bin/cucumber ./features/ "$@"
--- blackbox-test/support/env.rb
+++
@@ -1,4 +0,0 @@
-require 'excon'
-require 'json'
-
-
